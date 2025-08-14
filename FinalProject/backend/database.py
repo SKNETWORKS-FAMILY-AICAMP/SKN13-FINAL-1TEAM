539f8b8ca24325e0d1c72df16b9513f6b2f1a5ef
WooZhoon <jhwoo0412@gmail.com>
2025-08-14 16:09:46 +0900

    [지훈] database.py 수정

--- FinalProject/backend/database.py
+++ FinalProject/backend/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, ForeignKey, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime
