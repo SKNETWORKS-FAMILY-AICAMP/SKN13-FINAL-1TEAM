4ab1b6b467ef41ecddabd1793eddf02b3f24f6e3
WooZhoon <jhwoo0412@gmail.com>
2025-08-25 14:36:19 +0900

    [지훈] database.py 수정

--- FinalProject/backend/database.py
+++ FinalProject/backend/database.py
@@ -25,13 +25,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # --- 모델 정의 ---
 
-class Dept(Base):
-    __tablename__ = "dept"
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    deptname = Column(String(255),unique=True,ForeignKey=True)
-    dept_hot_Line = Column(String(255),unique=True)
-
-
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
@@ -41,7 +34,7 @@ class User(Base):
     email = Column(String(255), unique=True, index=True)
     created_at = Column(DateTime, default=datetime.now, index=True)
 
-    deptname = relationship("Dept", back_populates="users")
+    dept = Column(String(255))
     position = Column(String(255))
 
     chat_sessions = relationship("ChatSession", back_populates="user")
