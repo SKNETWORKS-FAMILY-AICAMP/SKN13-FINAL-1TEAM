85ab57bbd6f2a8d11806c37ccf4ba0c9da706a39
NGGW519 <srrd1357@gmail.com>
2025-09-01 15:48:09 +0900

    [지훈] 엔드포인트 편집중

--- FinalProject/backend/routers/calendar_routes.py
+++ FinalProject/backend/routers/calendar_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, stauts
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 from typing import List, Optional
 from sqlalchemy.orm import Session
