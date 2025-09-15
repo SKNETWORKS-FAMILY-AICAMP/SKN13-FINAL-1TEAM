8876d3e6b2a1624b655ca7e5fd5707e33814cf67
HCWDDD <codn1016@gmail.com>
2025-09-15 09:33:14 +0900

    [채우] 챗봇 css 수정

--- FinalProject/frontend-ui/src/components/Sidebar/ChatSummaryItem.jsx
+++ FinalProject/frontend-ui/src/components/Sidebar/ChatSummaryItem.jsx
@@ -1,12 +1,14 @@
 // ✅ ChatSummaryItem.jsx
+import { PiChatCircleDotsBold } from "react-icons/pi";
+
 export default function ChatSummaryItem({ title, onClick }) {
-  return (
-    <div
-      className="flex items-center gap-2 p-3 rounded-lg hover:bg-gray-100 cursor-pointer"
-      onClick={onClick}
-    >
-      <div className="text-gray-400">📄</div>
-      <div className="text-sm font-medium truncate">{title}</div>
-    </div>
-  );
+    return (
+        <div
+            className="flex items-center gap-2 p-3 rounded-lg hover:bg-gray-100 cursor-pointer"
+            onClick={onClick}
+        >
+            <PiChatCircleDotsBold size={24} />
+            <div className="text-sm font-medium truncate">{title}</div>
+        </div>
+    );
 }
